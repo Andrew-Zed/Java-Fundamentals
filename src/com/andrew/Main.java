9ce78333418ccc1206dace202bc3559a70f3458d
Andrew-Zed <andrew.awadyke@gmail.com>
2022-05-21 16:08:27 +0100

    first commit, constructor challenge

---
+++ src/com/andrew/Main.java
@@ -0,0 +1,17 @@
+package com.andrew;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Integer num1 = 1000, num2 = 1000;
+        System.out.println(num1 == num2);//1
+        Integer num3 = 20, num4 = 20;
+        System.out.println(num3 == num4);//2
+        System.out.println("Main 1");
+    }
+
+    public static void main(String args) {
+
+    }
+}
---
+++ src/com/andrew/contructorChallenge/Main.java
@@ -0,0 +1,9 @@
+package com.andrew.contructorChallenge;
+
+public class Main {
+    public static void main(String[] args) {
+    Account paulsAccount = new Account();
+    paulsAccount.withdrawal(500);
+
+    }
+}
