e2e9cc925c41b92e01fc59e59270a82bc33c1cfe
Andrew-Zed <andrew.awadyke@gmail.com>
2022-05-24 12:05:21 +0100

    interface practice implemented fully

--- src/com/andrew/interfaceChallenge/DeskPhone.java
+++ src/com/andrew/interfaceChallenge/DeskPhone.java
@@ -3,6 +3,11 @@ package com.andrew.interfaceChallenge;
 public class DeskPhone implements ITelephone {
     private int myNumber;
     private boolean isRinging;
+
+    public DeskPhone(int myNumber) {
+        this.myNumber = myNumber;
+    }
+
     @Override
     public void powerOn() {
         System.out.println("No action taking, desk phone does not have a power button");
@@ -10,21 +15,30 @@ public class DeskPhone implements ITelephone {
 
     @Override
     public void dial(int phoneNumber) {
-
+        System.out.println("Now Ringing " + phoneNumber + " on deskphone");
     }
 
     @Override
     public void answer() {
-
+        if(isRinging) {
+            System.out.println("Answering the desk phone");
+            isRinging = false;
+        }
     }
 
     @Override
     public boolean callPhone(int phoneNumber) {
-        return false;
+        if(phoneNumber == myNumber) {
+            isRinging = true;
+            System.out.println("Ring ring");
+        } else {
+            isRinging = false;
+        }
+        return isRinging;
     }
 
     @Override
     public boolean isRinging() {
-        return false;
+        return isRinging;
     }
 }
--- src/com/andrew/interfaceChallenge/Main.java
+++ src/com/andrew/interfaceChallenge/Main.java
@@ -2,6 +2,15 @@ package com.andrew.interfaceChallenge;
 
 public class Main {
     public static void main(String[] args) {
+        ITelephone andyPhone;
+        andyPhone = new DeskPhone(234802222);
+        andyPhone.powerOn();
+        andyPhone.callPhone(234802222);
+        andyPhone.answer();
 
+        andyPhone = new MobilePhone(234703513);
+        andyPhone.powerOn();
+        andyPhone.callPhone(234703513);
+        andyPhone.answer();
     }
 }
